d40a817eac602efeff9f04f7a8dd34740885decf
Qiusheng Wu <giswqs@gmail.com>
2025-02-22 23:04:59 -0500

    Add plot_batch and calc_stats functions

--- geoai/common.py
+++ geoai/common.py
@@ -1,9 +1,16 @@
 """The common module contains common functions and classes used by the other modules."""
 
 import os
+from collections.abc import Iterable
 from typing import Any, Dict, List, Optional, Tuple, Type, Union, Callable
+import matplotlib.pyplot as plt
+
 import torch
 import numpy as np
+from torch.utils.data import DataLoader
+from torchgeo.datasets import RasterDataset, stack_samples, unbind_samples, utils
+from torchgeo.samplers import RandomGeoSampler, Units
+from torchgeo.transforms import indices
 
 
 def viz_raster(
@@ -97,8 +104,6 @@ def viz_image(
         None
     """
 
-    import matplotlib.pyplot as plt
-
     if isinstance(image, torch.Tensor):
         image = image.cpu().numpy()
 
@@ -121,3 +126,154 @@ def viz_image(
         plt.axis("off")
     plt.show()
     plt.close()
+
+
+def plot_images(
+    images: Iterable[torch.Tensor],
+    axs: Iterable[plt.Axes],
+    chnls: List[int] = [2, 1, 0],
+    bright: float = 1.0,
+) -> None:
+    """
+    Plot a list of images.
+
+    Args:
+        images (Iterable[torch.Tensor]): The images to plot.
+        axs (Iterable[plt.Axes]): The axes to plot the images on.
+        chnls (List[int], optional): The channels to use for RGB. Defaults to [2, 1, 0].
+        bright (float, optional): The brightness factor. Defaults to 1.0.
+
+    Returns:
+        None
+    """
+    for img, ax in zip(images, axs):
+        arr = torch.clamp(bright * img, min=0, max=1).numpy()
+        rgb = arr.transpose(1, 2, 0)[:, :, chnls]
+        ax.imshow(rgb)
+        ax.axis("off")
+
+
+def plot_masks(
+    masks: Iterable[torch.Tensor], axs: Iterable[plt.Axes], cmap: str = "Blues"
+) -> None:
+    """
+    Plot a list of masks.
+
+    Args:
+        masks (Iterable[torch.Tensor]): The masks to plot.
+        axs (Iterable[plt.Axes]): The axes to plot the masks on.
+        cmap (str, optional): The colormap to use. Defaults to "Blues".
+
+    Returns:
+        None
+    """
+    for mask, ax in zip(masks, axs):
+        ax.imshow(mask.squeeze().numpy(), cmap=cmap)
+        ax.axis("off")
+
+
+def plot_batch(
+    batch: Dict[str, Any],
+    bright: float = 1.0,
+    cols: int = 4,
+    width: int = 5,
+    chnls: List[int] = [2, 1, 0],
+    cmap: str = "Blues",
+) -> None:
+    """
+    Plot a batch of images and masks. This function is adapted from the plot_batch()
+    function in the torchgeo library at
+    https://torchgeo.readthedocs.io/en/stable/tutorials/earth_surface_water.html
+    Credit to the torchgeo developers for the original implementation.
+
+    Args:
+        batch (Dict[str, Any]): The batch containing images and masks.
+        bright (float, optional): The brightness factor. Defaults to 1.0.
+        cols (int, optional): The number of columns in the plot grid. Defaults to 4.
+        width (int, optional): The width of each plot. Defaults to 5.
+        chnls (List[int], optional): The channels to use for RGB. Defaults to [2, 1, 0].
+        cmap (str, optional): The colormap to use for masks. Defaults to "Blues".
+
+    Returns:
+        None
+    """
+    # Get the samples and the number of items in the batch
+    samples = unbind_samples(batch.copy())
+
+    # if batch contains images and masks, the number of images will be doubled
+    n = 2 * len(samples) if ("image" in batch) and ("mask" in batch) else len(samples)
+
+    # calculate the number of rows in the grid
+    rows = n // cols + (1 if n % cols != 0 else 0)
+
+    # create a grid
+    _, axs = plt.subplots(rows, cols, figsize=(cols * width, rows * width))
+
+    if ("image" in batch) and ("mask" in batch):
+        # plot the images on the even axis
+        plot_images(
+            images=map(lambda x: x["image"], samples),
+            axs=axs.reshape(-1)[::2],
+            chnls=chnls,
+            bright=bright,
+        )
+
+        # plot the masks on the odd axis
+        plot_masks(masks=map(lambda x: x["mask"], samples), axs=axs.reshape(-1)[1::2])
+
+    else:
+        if "image" in batch:
+            plot_images(
+                images=map(lambda x: x["image"], samples),
+                axs=axs.reshape(-1),
+                chnls=chnls,
+                bright=bright,
+            )
+
+        elif "mask" in batch:
+            plot_masks(
+                masks=map(lambda x: x["mask"], samples), axs=axs.reshape(-1), cmap=cmap
+            )
+
+
+def calc_stats(
+    dataset: RasterDataset, divide_by: float = 1.0
+) -> Tuple[np.ndarray, np.ndarray]:
+    """
+    Calculate the statistics (mean and std) for the entire dataset.
+
+    This function is adapted from the plot_batch() function in the torchgeo library at
+    https://torchgeo.readthedocs.io/en/stable/tutorials/earth_surface_water.html.
+    Credit to the torchgeo developers for the original implementation.
+
+    Warning: This is an approximation. The correct value should take into account the
+    mean for the whole dataset for computing individual stds.
+
+    Args:
+        dataset (RasterDataset): The dataset to calculate statistics for.
+        divide_by (float, optional): The value to divide the image data by. Defaults to 1.0.
+
+    Returns:
+        Tuple[np.ndarray, np.ndarray]: The mean and standard deviation for each band.
+    """
+    import rasterio as rio
+
+    # To avoid loading the entire dataset in memory, we will loop through each img
+    # The filenames will be retrieved from the dataset's rtree index
+    files = [
+        item.object
+        for item in dataset.index.intersection(dataset.index.bounds, objects=True)
+    ]
+
+    # Resetting statistics
+    accum_mean = 0
+    accum_std = 0
+
+    for file in files:
+        img = rio.open(file).read() / divide_by  # type: ignore
+        accum_mean += img.reshape((img.shape[0], -1)).mean(axis=1)
+        accum_std += img.reshape((img.shape[0], -1)).std(axis=1)
+
+    # at the end, we shall have 2 vectors with length n=chnls
+    # we will average them considering the number of images
+    return accum_mean / len(files), accum_std / len(files)
