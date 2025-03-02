7245f3ab969ca17997dd94e29144fbd69db6c80e
Qiusheng Wu <giswqs@gmail.com>
2025-03-01 16:32:52 -0800

    Add set_proj_lib_path function (#21)

--- geoai/__init__.py
+++ geoai/__init__.py
@@ -5,4 +5,29 @@ __email__ = "giswqs@gmail.com"
 __version__ = "0.1.7"
 
 
+import os
+import sys
+
+
+def set_proj_lib_path():
+    """Set the PROJ_LIB environment variable based on the current conda environment."""
+    try:
+        # Get conda environment path
+        conda_env_path = os.environ.get("CONDA_PREFIX") or sys.prefix
+
+        # Set PROJ_LIB environment variable
+        proj_path = os.path.join(conda_env_path, "share", "proj")
+        gdal_path = os.path.join(conda_env_path, "share", "gdal")
+
+        # Check if the directory exists before setting
+        if os.path.exists(proj_path):
+            os.environ["PROJ_LIB"] = proj_path
+        if os.path.exists(gdal_path):
+            os.environ["GDAL_DATA"] = gdal_path
+    except Exception as e:
+        print(e)
+        return
+
+
+set_proj_lib_path()
 from .geoai import *
