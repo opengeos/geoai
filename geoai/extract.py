67b60d0cf48b2969c2b222e5b59242ddabc0222e
Qiusheng Wu <giswqs@gmail.com>
2025-03-05 22:54:42 -0500

    Add ShipDetector

--- geoai/extract.py
+++ geoai/extract.py
@@ -1855,3 +1855,28 @@ class CarDetector(ObjectDetector):
         super().__init__(
             model_path=model_path, repo_id=repo_id, model=model, device=device
         )
+
+
+class ShipDetector(ObjectDetector):
+    """
+    Ship detection using a pre-trained Mask R-CNN model.
+
+    This class extends the
+    `ObjectDetector` class with additional methods for ship detection."
+    """
+
+    def __init__(
+        self, model_path="ship_detection.pth", repo_id=None, model=None, device=None
+    ):
+        """
+        Initialize the object extractor.
+
+        Args:
+            model_path: Path to the .pth model file.
+            repo_id: Repo ID for loading models from the Hub.
+            model: Custom model to use for inference.
+            device: Device to use for inference ('cuda:0', 'cpu', etc.).
+        """
+        super().__init__(
+            model_path=model_path, repo_id=repo_id, model=model, device=device
+        )
