b174a0c8fdb7d01cb15cef1cb072ccc88b7a7893
Pratik-Doshi-99 <newswanprivate@gmail.com>
2025-08-27 05:03:10 +0000

    added test cases and test code

--- TestClay_GeoAI.py
+++ TestClay_GeoAI.py
@@ -1,6 +1,6 @@
 import sys
 import os
-sys.path.append(os.path.join(os.path.dirname(__file__), 'geoai'))
+#sys.path.append(os.path.join(os.path.dirname(__file__), 'geoai'))
 
 import geopandas as gpd
 import numpy as np
